3ddb44268bf567c3d9dbcf1100fc6455584d9522
nazim nasser <badih123khoder@gmail.com>
2022-02-26 19:48:37 +0200

    step-4

--- Step_4.php
+++ Step_4.php
@@ -1,7 +1,15 @@
 <?php
- $variable_fname = "badih ";
- $variable_lname = "khoder";
+$name = "badih ";
+$postion = "student";
+$github_url = "www.github.com/badih-khoder";
+
+$VAL = "badih ";
+$VAL = "student";
+$VAL = "www.github.com/badih-khoder";
+
+$a = "hello I'm " . $name . " I'm a " . $postion . " please check my github link " . $github_url;
+$b = "hello I'm   $name   I'm a   $postion   please check my github link  $github_url";
 
 
- echo  $variable_fname, $variable_lname;
-?>
+
+echo "$a, \n $b,\n";
