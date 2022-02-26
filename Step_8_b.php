8c05ca611fd897e1b6bc53f0aa34ba3d1123f7f2
nazim nasser <badih123khoder@gmail.com>
2022-02-26 22:15:39 +0200

    step-8-b

---
+++ Step_8_b.php
@@ -0,0 +1,7 @@
+<?php
+
+$egss  = array('balade', 'mazere3');
+$milk  = array('Fresh', 'Taanayel');
+$water_pack  = array('Tanoureen', 'Reem');
+
+echo "Hey Sir, Please I need 1 pack of $egss[0] eggs and 3 $water_pack[1] Water Pack";
