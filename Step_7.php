fbd0cf35532b1eebdbd54540e9294fab11216ad4
nazim nasser <badih123khoder@gmail.com>
2022-02-26 21:41:25 +0200

    step-7

---
+++ Step_7.php
@@ -0,0 +1,12 @@
+<?php
+
+$birth_year = 1999;
+$current_year = 2022;
+$age = $current_year - $birth_year;
+
+
+if ($age < 18) {
+    echo "You still a kid, go and play GTA";
+} else {
+    echo "you can drive";
+}
