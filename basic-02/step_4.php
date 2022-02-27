eb762389f082360f9fe77d0cc0408ef483d71515
nazim nasser <badih123khoder@gmail.com>
2022-02-27 14:07:53 +0200

    step-4

--- basic-02/step_4.php
+++ basic-02/step_4.php
@@ -1,6 +1,21 @@
-
-
-
+<?php
+function armstrong_number($num) {
+$rem=0;
+$total=0;
+$x=$num;
+while($x !=0){
+  $rem =$x % 10;
+  $total = $total + $rem * $rem * $rem;
+  $x=$x / 10;
+}
+  if ($total == $x) {
+    echo "True \n";
+  } else {
+    echo "False\n";
+  }
+}
+armstrong_number (10)
+?>
 
 
 
